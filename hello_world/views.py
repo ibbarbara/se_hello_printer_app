90b0aa9893c9b24c1af9250cdd3a9ce8d76b449f
ibbarbara <barbara.barszczak@gmail.com>
2021-04-24 10:55:45 +0200

    Zmienione imie na Barbara w views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Barbara"
 msg = "Hello World!"
 
 @app.route('/')
