1907bb2b1bf353f3a4c1ca6a6f76a5bd944dfad0
ibbarbara <barbara.barszczak@gmail.com>
2021-04-25 11:07:42 +0200

    Dodanie XML

--- test/test_views.py
+++ test/test_views.py
@@ -16,3 +16,8 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEqual(b'{ "imie":"Barbara", "msg":"Hello World!"}', rv.data)
+
+# dodawanie XML    
+     def test_msg_with_output(self):
+        rv = self.app.get('/?output=xml')
+        self.assertEqual(b'{ "imie":"Barbara", "msg":"Hello World!"}', rv.data)
